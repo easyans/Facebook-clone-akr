efada0bcb216c6c01ae9a818e5877217c2e8b4ab
Aakash kumar raj <ar0829@srmist.edu.in>
2023-01-14 18:56:26 +0530

    first commit

---
+++ src/HomePage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+import HomeContainer from './components/HomeContainer'
+import LeftContainer from './components/LeftContainer'
+import RightContainer from './components/RightContainer'
+
+function HomePage() {
+    return (
+        <Home>
+            <LeftContainer />
+            <HomeContainer />
+            <RightContainer />
+        </Home>
+    )
+}
+
+export default HomePage
+
+const Home = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 62px;
+    color: white;
+    flex: 1;
+    overflow: hidden
+`
